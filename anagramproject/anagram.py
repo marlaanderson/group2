446c824a95e821a701266283db9cdaef54b0a923
Kevin Ramirez <kevinramirez723@gmail.com>
2018-08-07 18:21:26 -0500

    update 1

--- anagramproject/anagram.py
+++ anagramproject/anagram.py
@@ -1,12 +1,20 @@
 #
+from collections import defaultdict
 		
 def read():
 	infile = open("eng_dict.txt", "r")
 	outfile = open("anagramtest.txt", "w")
+	table = defaultdict(list)
 	for line in infile:			#loops for each individual line in infile
 		if len(line) > 8:		#checks if line is greater than 7 characters (8 because of newline character)
-			sort = "".join(sorted(line.lower().strip()))
-			print("{} {:>25}" .format(sort, line.lower().strip()), file=outfile)	#stores results in anagramtest file
+			key = "".join(sorted(line.lower().strip()))
+		#print("{} {:>25}" .format(key, line.lower().strip()), file=outfile)	#stores results in anagramtest file
+			table[key].append(line.lower().strip())
+	for keys,values in table.items():
+		print(keys, file=outfile)
+		print(values, file=outfile)
+		print("", file=outfile)
+	print(len(table))
 			
 	infile.close()
 	outfile.close()	
