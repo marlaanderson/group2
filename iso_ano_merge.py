01856f4b9e9e6d2bab3dc85a9a29061c2195ff01
ctetrick <40645147+ctetrick@users.noreply.github.com>
2018-08-08 15:13:55 -0500

    Update iso_ano_merge.py

--- iso_ano_merge.py
+++ iso_ano_merge.py
@@ -1,43 +1,46 @@
-#The goal of this program is to merge the seperate isogram and anagram programs into a single program
-def iso_ano_merge():
-from collections import defaultdict				#Imports defaultdict: It overrides one method and adds one writable instance variable. The rest of its properties are the same as dict.
+from collections import defaultdict
 
-table = defaultdict(list)						#Creates a defaultdict with lists as values.
-		
-def read():
-	infile = open("eng_dict.txt", "r")			#Open dictionary to analyze.
-	outfile = open("anagramtest.txt", "w")		#Open new file to write outcome to.
-	anagram_permute(infile, outfile, table)		#Runs anagram creation.
-	isograms()
-  infile.close()
-	outfile.close()
+table = defaultdict(list)
+
+def iso_ano_merge():
+						#Creates a defaultdict with lists as values.	
+    infile = open("eng_dict.txt", "r")			#Open dictionary to analyze.
+    outfile = open("anagramtest.txt", "w")
+    outfile2 = open("isogramtest.txt","w")
+    keylist = anagram_permute(infile, outfile, table)		#Runs anagram creation.
+    isograms(keylist, outfile2)
+    infile.close()
+    outfile.close()
+    outfile2.close()
 
 def anagram_permute(infile, outfile, table):
-	no_dupes = list(sorted(set(infile.read().lower().strip().split())))				#Creates a new list identical to eng_dict except every element is unique and lowercase.
+    no_dupes = list(sorted(set(infile.read().lower().strip().split())))				#Creates a new list identical to eng_dict except every element is unique and lowercase.
 																					#Words like cardigan were counted double because uppercase and lowercase version were counted as anagrams of each other.
+    for line in no_dupes:															#Loops for each individual element in no_dupes.
+        if len(line) > 7:															#Checks if line is greater than 7 characters.
+            key = "".join(sorted(line))												#Creates a key out of the alphabetized lowercased letters of each word and strips the newline characters.
+            table[key].append(line)													#A new element will then be added to the value list of the specific key being called.
 
-	for line in no_dupes:															#Loops for each individual element in no_dupes.
-		if len(line) > 7:															#Checks if line is greater than 7 characters.
-			key = "".join(sorted(line))												#Creates a key out of the alphabetized lowercased letters of each word and strips the newline characters.
-			table[key].append(line)													#A new element will then be added to the value list of the specific key being called.
+    keylist = list(table.keys())													#Creates a list of the keys.
+    keylist.sort(key=len)															#Sorts key list by key length.
+    keylist = sorted(keylist, key=lambda x: len(table[x]))							#Sorts key list by length of value list (amount of anagrams).
+    count = 0																		#Initializes count variable
+    for key in keylist:																#Iterates throught the entire list.
+        if len(table[key]) > 1:														#Will only print if there is atleast an anagram pair.
+            count+=1																#Keeps track of how many keys there are are in total which have atleast an anagram pair.
+            print("{:<25}| {}".format(key, ", ".join(table[key])), file=outfile)	#Prints out key and the list of anagrams associated with that key.
+    print("\nThere are this many anagrams:", count, file=outfile)					#Prints Final tally of anagram families.
+    return keylist
 
-	keylist = list(table.keys())													#Creates a list of the keys.
-	keylist.sort(key=len)															#Sorts key list by key length.
-	keylist = sorted(keylist, key=lambda x: len(table[x]))							#Sorts key list by length of value list (amount of anagrams).
-	count = 0																		#Initializes count variable
-	for key in keylist:																#Iterates throught the entire list.
-		if len(table[key]) > 1:														#Will only print if there is atleast an anagram pair.
-			count+=1																#Keeps track of how many keys there are are in total which have atleast an anagram pair.
-			print("{:<25}| {}".format(key, ", ".join(table[key])), file=outfile)	#Prints out key and the list of anagrams associated with that key.
-	
-	print("\nThere are this many anagrams:", count, file=outfile)					#Prints Final tally of anagram families.
-def isograms(outfile): 
-    infile = open("outfile", "r")
-    count = 0
-    for line in infile:
-        line = line.lower().strip()
+def isograms(keylist, outfile2): 
+    count = 0	
+    for line in keylist:
+        if len(table[line]) > 1:
+            line = line.lower().strip()
             if len(set(line)) == len(line):
                 count+=1
-                print("There are %s isograms!"%(line))
+                print("{:<25}| {}".format(line, ", ".join(table[line])), file=outfile2)
+    print("There are %s isograms!"%(count), file=outfile2)
     print(count)
-read()
+iso_ano_merge()
+
