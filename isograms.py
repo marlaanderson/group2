c665cea1cea9a93292436b369f2836869da77ad3
marlaanderson <mfanderson@uchicago.edu>
2018-08-08 16:25:30 -0500

    isogram changes

--- isograms.py
+++ isograms.py
@@ -1,5 +1,4 @@
 def isograms():
-<<<<<<< HEAD
     infile = open("eng_dict.txt", "r")
     count = 0
     for line in infile:
@@ -10,15 +9,3 @@ def isograms():
                 print(line)
     print("There are %s isograms!"%(count))
 isograms()
-=======
-    infile = open("/Users/Marla/Desktop/CAAP-CS/final_project/group2/eng_dict.txt", "r")
-    count = 0
-    for line in infile:
-        line = line.lower().strip()
-        if len(line) > 8:
-            if len(set(line)) == len(line):
-                count+=1
-                print("There are %s isograms!"%(line))
-    print(count)
-isograms()
->>>>>>> d0cbd453967965f8bbf9c853e0a71e4e70be9c87
