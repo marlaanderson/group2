659b5cb9d76b701e287ab09b63990ca329271f1f
Kevin Ramirez <kevinramirez723@gmail.com>
2018-08-07 21:25:01 -0500

    update 3

--- anagramproject/anagram.py
+++ anagramproject/anagram.py
@@ -1,29 +1,31 @@
-#
+#Kevin Ramirez
+
 from collections import defaultdict
 		
 def read():
 	infile = open("eng_dict.txt", "r")
 	outfile = open("anagramtest.txt", "w")
+	anagram_permute(infile, outfile)
+	infile.close()
+	outfile.close()
+
+def anagram_permute(infile, outfile):
 	table = defaultdict(list)
+	c = 0
 	for line in infile:			#loops for each individual line in infile
+		c+=1
 		if len(line) > 8:		#checks if line is greater than 7 characters (8 because of newline character)
 			key = "".join(sorted(line.lower().strip()))
-		#print("{} {:>25}" .format(key, line.lower().strip()), file=outfile)	#stores results in anagramtest file
 			table[key].append(line.lower().strip())
-			
+
 	keylist = list(table.keys())
 	keylist.sort(key=len)
+	count = 0
 	for key in keylist:
-		print(key, table[key], file=outfile)
-			
-	#for keys,values in table.items():
-		#print(keys, file=outfile)
-		#print(values, file=outfile)
-		#print("", file=outfile)
+		if len(table[key]) > 1:
+			count+=1
+			print("{}: {}".format(key, ", ".join(table[key])), file=outfile)
 	
-	print(len(table))
-			
-	infile.close()
-	outfile.close()	
+	print("\nAMOUNT OF WORDS:", count, file=outfile)
 	
 read()
