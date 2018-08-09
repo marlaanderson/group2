2e1033282d2eafc0a96175e35c1ef909ace8aaf5
marlaanderson <40746028+marlaanderson@users.noreply.github.com>
2018-08-08 21:07:42 -0500

    adding comments

--- isograms.py
+++ isograms.py
@@ -2,9 +2,9 @@ def isograms():
     infile = open("eng_dict.txt", "r")
     count = 0
     for line in infile:
-        line = line.lower().strip()
+        line = line.lower().strip() #makes string lower case and strips it 
         if len(line) > 7:
-            if len(set(line)) == len(line):
+            if len(set(line)) == len(line): #checks if the length of the set of line is equal to the length of line
                 count+=1
                 print(line)
     print("There are %s isograms!"%(count))
